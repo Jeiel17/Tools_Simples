e58143b889bdfed57299ba19cbf433ffb21892a9
Jeiel <Jeiel17@protonmail.com>
2021-04-26 11:01:59 -0300

    Add files via upload

---
+++ Banco de dados simples.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Primeiro deveremos importar o 'numpy', a qual será capaz de armazenar os dados:
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[15]:
+
+
+Jeiel_gosta = ['Python', 'HTML']
+João_gosta = ['Java', 'JavaScript', 'PHP']
+Ana_gosta = ['Python', 'Java', 'C', 'PHP', 'JavaScript', 'Swift']
+[Ana_gosta]
+
+
+# In[ ]:
+
+
+# Dados foram gravados. Também foi possível saber o gosto de uma pessoa.
+
